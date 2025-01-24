72c049c35db259592a888ee239bbbe8a56af0225
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-24 19:05:51 +0330

    css myself

--- script.js
+++
@@ -1,8 +0,0 @@
-var index=0;
-function changeColors(){
-    var colors=["red","blue","green","yellow","purple","pink","black","white","brown","orange"];
-    document.querySelector('body').style.background = colors[index++];
-    if(index>colors.length - 1){
-        index=0;
-    }
-}
